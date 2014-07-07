e4394c09ed6d0b83fdb5ffffb59311fa4361cd7f
Jürgen Scholz <juergen@scholz-email.de>
2014-07-07 08:53:44 +0200

    Added 2 new functions

--- php/MachinecoinRPC.php
+++ php/MachinecoinRPC.php
@@ -38,138 +38,159 @@ require_once dirname(  __FILE__ ) . '/jsonRPCClient.php';
 
 class Machinecoin {
 
-  private $client;
+      private $client;
 
-    /** 
-     * Create client to connect on init
-     * @param $config array of parameters $host, $port, $user, $pass
-     */
+      /** 
+       * Create client to connect on init
+       * @param $config array of parameters $host, $port, $user, $pass
+       */
 
-    function __construct( $config ) {
+      function __construct( $config ) {
 
-      $connect_string = sprintf ( 'http://%s:%s@%s:%s/',
+        $connect_string = sprintf ( 'http://%s:%s@%s:%s/',
         $config['user'],
         $config['pass'],
         $config['host'],
         $config['port']);
 
-      // internal client to use for connection
-      $this->client = new jsonRPCClient( $connect_string );
-    }
+        // internal client to use for connection
+        $this->client = new jsonRPCClient( $connect_string );
+      }
 
 
-    /**
-     * Creates or Retrievs a Machinecoin address for a account name
-     * An account is just a string used as key to identify account,
-     * A Machinecoin address is return which can receive coins
-     *
-     * @param string $account some string used as key to account
-     * @return string machinecoin address 
-     */
-    function get_address( $account ) {
-      return $this->client->getaccountaddress( $account );
-    }
+      /**
+       * Given a Machinecoin address returns the account name
+       *
+       * @param string $address machinecoin addresss
+       * @return string account name key
+       */ 
+       function getaccount( $address ) {
+         return $this->client->getaccount( $address );
+       }
 
 
-    /**
-     * Given a Machinecoin address returns the account name
-     *
-     * @param string $address machinecoin addresss
-     * @return string account name key
-     */ 
-     function get_account( $address ) {
-      return $this->client->getaccount( $address );
-     }
+      /**
+       * Creates or Retrievs a Machinecoin address for a account name
+       * An account is just a string used as key to identify account,
+       * A Machinecoin address is return which can receive coins
+       *
+       * @param string $account some string used as key to account
+       * @return string machinecoin address 
+       */
+       function getaccountaddress( $account ) {
+         return $this->client->getaccountaddress( $account );
+       }
 
 
-    /**
-     * Create new address for account, recommended to include
-     * account name for further API use.
-     *
-     * @param string $account account name
-     * @return string Machinecoin address
-     */
-     function get_new_address( $account='' ) {
-       return $this->client->getnewaddress( $account );
-     }
+      /**
+       * Get balance for given account
+       *
+       * @param string $account account name
+       * @return float account balance
+       */
+       function getbalance( $account, $minconf=1 ) {
+         return $this->client->getbalance( $account, $minconf );
+       }
 
 
-    /**
-     * Get list of all accounts on in this Machinecoind wallet
-     *
-     * @return array strings of account => balance
-     */
-     function list_accounts() {
-       return $this->client->listaccounts();
-     }
+      /**
+       * Get the proof-of-work difficulty 
+       *
+       * @return string Machinecoin difficulty
+       */
+       function getdifficulty() {
+         return $this->client->getdifficulty();
+       }
 
 
-    /**
-     * Get the details of a transaction
-     *
-     * @param string $txid transaction id
-     * @return array describing the transaction
-     */
-     function get_transaction( $txid ) {
-       return $this->client->gettransaction( $txid );
-     }
+      /**
+       * Get the the number of blocks in the longest block chain 
+       *
+       * @return string Machinecoin blocknumber
+       */
+       function getblockcount() {
+         return $this->client->getblockcount();
+       }
 
 
-    /**
-     * Associate Machinecoin address to account string
-     *
-     * @param string $address Machinecoin address
-     * @param string $account account string
-     */
-    function set_account( $address, $account ) {
-        return $this->client->setaccount($address, $account);
-    }
+      /**
+       * Create new address for account, recommended to include
+       * account name for further API use.
+       *
+       * @param string $account account name
+       * @return string Machinecoin address
+       */
+       function getnewaddress( $account='' ) {
+         return $this->client->getnewaddress( $account );
+       }
 
 
-    /**
-     * Get balance for given account
-     *
-     * @param string $account account name
-     * @return float account balance
-     */
-    function get_balance( $account, $minconf=1 ) {
-        return $this->client->getbalance( $account, $minconf );
-    }
+      /**
+       * Get the details of a transaction
+       *
+       * @param string $txid transaction id
+       * @return array describing the transaction
+       */
+       function gettransaction( $txid ) {
+         return $this->client->gettransaction( $txid );
+       }
 
 
-    /**
-     * Move coins from one account on wallet to another
-     * Both accounts are local to this Machinecoind instance
-     *
-     * @param string $account_from account moving from
-     * @param string $account_to account moving to
-     * @param float $amount amount of coins to move
-     * @return
-     */
-    function move( $account_from, $account_to, $amount ) {
-        return $this->client->move( $account_from, $account_to, $amount );
-    }
+      /**
+       * Get list of all accounts on in this Machinecoind wallet
+       *
+       * @return array strings of account => balance
+       */
+       function listaccounts() {
+         return $this->client->listaccounts();
+       }
 
 
-    /**
-     * Send coins to any Machinecoin Address
-     *
-     * @param string $account account sending coins from
-     * @param string $to_address Machinecoin address sending to
-     * @param float $amount amount of coins to send
-     * @return string txid
-     */
-    function send( $account, $to_address, $amount ) {
-        $txid = $this->client->sendfrom( $account, $to_address, $amount );  
-        return $txid;
-    }
+      /**
+       * Move coins from one account on wallet to another
+       * Both accounts are local to this Machinecoind instance
+       *
+       * @param string $account_from account moving from
+       * @param string $account_to account moving to
+       * @param float $amount amount of coins to move
+       * @return
+       */
+       function move( $account_from, $account_to, $amount ) {
+         return $this->client->move( $account_from, $account_to, $amount );
+       }
+
+
+      /**
+       * Send coins to any Machinecoin Address
+       *
+       * @param string $account account sending coins from
+       * @param string $to_address Machinecoin address sending to
+       * @param float $amount amount of coins to send
+       * @return string txid
+       */
+       function sendfrom( $account, $to_address, $amount ) {
+         $txid = $this->client->sendfrom( $account, $to_address, $amount );  
+         return $txid;
+       }
+
+
+      /**
+       * Associate Machinecoin address to account string
+       *
+       * @param string $address Machinecoin address
+       * @param string $account account string
+       */
+       function setaccount( $address, $account ) {
+         return $this->client->setaccount($address, $account);
+       }
+
 
       /**
        * Validate a given Machinecoin Address
        * @param string $address to validate
        * @return array with the properties of the address
        */
-       function validate_address( $address ) {
+       function validateaddress( $address ) {
          return $this->client->validateaddress($address);
        }   
 }
