cce067979be01b198da13c43754f423342f007b9
Jürgen Scholz <juergen@scholz-email.de>
2014-07-20 00:50:45 +0200

    Small changes

--- php/MacRPC.php
+++ php/MacRPC.php
@@ -36,7 +36,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 require_once dirname(  __FILE__ ) . '/jsonRPCClient.php';
 
-class Machinecoin {
+class MacRPC {
 
       private $client;
 
--- php/example.php
+++ php/example.php
@@ -13,24 +13,24 @@ $config = array
 );
 
 // create client connection
-$machinecoin = new machinecoin( $config );
+$MacRPC = new MacRPC( $config );
 
 // create a new address
-$address = $machinecoin->getaddress( 'gitju' );
+$address = $MacRPC->getaddress( 'gitju' );
 print( 'Address: ' . $address . ' ');
 
 // list accounts in wallet
-print_r( $machinecoin->listaccounts() );
+print_r( $MacRPC->listaccounts() );
 
 // get balance in wallet
-print( 'gitju: ' . $machinecoin->getbalance( 'gitju' ) );
+print( 'gitju: ' . $MacRPC->getbalance( 'gitju' ) );
 
 // move money from accounts in wallet
 // moves from 'nico' to account 'gitju'
-$machinecoin->move( 'nico', 'gitju', 10000 );
+$MacRPC->move( 'nico', 'gitju', 10000 );
 
 // send money externally (withdrawal?)
 // send from account to external address
-$machinecoin->sendfrom( 'gitju', 'MCsEypMLMtBkAuqzevYxeFxiSUx5eWAU3y', 100 );
+$MacRPC->sendfrom( 'gitju', 'MCsEypMLMtBkAuqzevYxeFxiSUx5eWAU3y', 100 );
 
 ?>
